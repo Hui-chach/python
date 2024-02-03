86c6b82af9c36ad8517e06edf406f611131e1f34
Hui-chach <153981044+Hui-chach@users.noreply.github.com>
2024-02-03 16:56:46 +0900

    Add files via upload

---
+++ 와우 ㅡㅛ 구쇼.py
@@ -0,0 +1,7 @@
+sum = 0
+avg = 0
+for i in range(5):
+    n = int(input("키입력.:"))
+    sum += n
+print("총합은 : %f" %sum)
+print("평균은: %.2f" %(sum/5))
---
+++ 패트릭.py
@@ -0,0 +1,2 @@
+for name in List :
+    print("%s, Congratulation!!!!!" % name)
